5ad4adfa3e7a6d4831d37ce3dca3a9ff18a7eebc
luis_nieto <tenseisb4@gmail.com>
2023-01-12 21:13:32 -0500

    Iniciando el repositorio con la actividad #1 del curso java

---
+++ src/java_basico/Actividad_1.java
@@ -0,0 +1,36 @@
+package java_basico;
+
+public class Actividad_1 {
+    public static void main (String[] args){
+        //ENTEROS
+
+        byte entero1 = 1;
+        short entero2 = 11;
+        int entero3 = 50;
+        long entero4 = 100;
+
+        System.out.println("Numeros enteros: "+entero1+", "+entero2+", "+entero3+", "+entero4);
+
+        //DECIMALES
+
+        float decimales1 =4.9f;
+        double decimales2 = 450.4d;
+
+        System.out.println("Numeros decimales: "+decimales1+", "+decimales2);
+
+        //TIPO TEXTO
+
+            //CADENA DE TEXTO
+        String cadena_texto = "hola mundo";
+
+            //CARACTER
+        char caracter = 'a';
+
+        System.out.println("TIPO TEXTO: "+cadena_texto+", "+caracter);
+
+        //BOOLEANOS
+        boolean opcion1 = true;
+        boolean opcion2 = false;
+
+    }
+}
