668fdf7b2b0f8b117015bd6eb4684afe383621cf
luis_nieto <tenseisb4@gmail.com>
2023-01-12 22:59:28 -0500

    Segunda actividad #2 del curso

---
+++ src/java_basico/Actividad_2.java
@@ -0,0 +1,19 @@
+package java_basico;
+
+import java.util.Scanner;
+
+public class Actividad_2 {
+    public static void main(String[] args) {
+        Scanner dato = new Scanner(System.in);
+        int valor_producto;double resultado, iva;
+        System.out.println("Digite el valor del producto");
+        valor_producto= dato.nextInt();
+        System.out.println("Digite el porcentaje de IVA del producto como numero entero.");
+        iva= dato.nextInt();
+
+        resultado = valor_producto + (valor_producto * 1 / iva);
+        System.out.println("Valor del Producto con el iva incluido: "+resultado);
+
+    }
+
+}
