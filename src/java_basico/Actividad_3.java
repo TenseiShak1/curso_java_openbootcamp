afc371cfa27ccf22b9d7af634131d17997bdb4c1
luis_nieto <tenseisb4@gmail.com>
2023-01-15 17:41:41 -0500

    Tercera actividad del curso

---
+++ src/java_basico/Actividad_3.java
@@ -0,0 +1,11 @@
+package java_basico;
+public class Actividad_3 {
+    public static void main(String[] args) {
+        String [] palabras = {"Tocar","Abrir","Cerrar","Empujar","Jalar"};
+        //Aplique un bucle FOR-EACH
+        for (String palabra : palabras) {
+            System.out.print(palabra);
+        }
+
+    }
+}
